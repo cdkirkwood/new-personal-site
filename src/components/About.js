8276c42885e48a215a57507f85fe37737cdea106
Connor Kirkwood <cdkirkwood@gmail.com>
2018-09-23 17:44:31 -0400

    fixed About typo

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import '../componentStyles/About.css'
 const About = () => (
   <div className="about-container">
     <h3>Little bit about me...</h3>
-    <p>After spending some time in the service industry and sales, I began to feel to urge to seek out a new path. I started coding about a year ago and immediately fell in love with JavaScript. The challenge of learning how to code and develop websites brought a new sense of energy and fulfillment into my life. I am looking to continue this journey by officially starting a career in web development. I am looking to work for a small or medium-sized company where I can continue to learn and thrive in an exciting environment.</p>
+    <p>After spending some time in the service industry and sales, I began to feel the urge to seek out a new path. I started coding about a year ago and immediately fell in love with JavaScript. The challenge of learning how to code and develop websites brought a new sense of energy and fulfillment into my life. I am looking to continue this journey by officially starting a career in web development. I am looking to work for a small or medium-sized company where I can continue to learn and thrive in an exciting environment.</p>
   </div>
 )
 
